13339ce4a2901aa3e1b3103da411ccf2259789f8
FrankTheTank <franklin@bitsmasher.net>
2019-02-13 21:15:43 -0500

    fix indents

--- my_resume.py
+++ my_resume.py
@@ -8,14 +8,13 @@ def render_static():
   return render_template('index.html', title = 'Franklin D. Resume')
 
 @my_resume.route('/download')
-  def download():
-    file = open('doc/my_resume.docx','r')
-  	returnfile = file.read().encode('latin-1')
-  	file.close()
-  	return Response(returnfile,
-  	  mimetype="text/docx",
-  	  headers={"Content-disposition":
-  	           "attachment; filename=doc/my_resume.docx"})
+def download():
+  file = open('doc/my_resume.docx','r')
+  returnfile = file.read().encode('latin-1')
+  file.close()
+  return Response(returnfile,
+  	 mimetype="text/docx",
+  	 headers={"Content-disposition": "attachment; filename=doc/my_resume.docx"})
 
 if __name__ == '__main__':
   bashCommand = "make html"
