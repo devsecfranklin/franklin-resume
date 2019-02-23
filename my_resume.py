609f49a3030dd03a94cf9c4dc61ddcb0162681a7
FrankTheTank <franklin@bitsmasher.net>
2019-02-23 11:09:26 -0500

    update 404

--- my_resume.py
+++ my_resume.py
@@ -15,6 +15,11 @@ def download():
   file.close()
   return Response(returnfile, mimetype="application/vnd.openxmlformats-officedocument.wordprocessingml.document", headers={"Content-disposition": "attachment; filename=my_resume.docx"})
 
+@my_resume.errorhandler(404)
+def page_not_found(e):
+  # note that we set the 404 status explicitly
+  return render_template('404.html'), 404
+
 if __name__ == '__main__':
   bashCommand = "make html"
   process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
