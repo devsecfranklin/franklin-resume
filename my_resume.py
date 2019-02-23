1b2562724657542f8768d47a995e87a035f323ec
FrankTheTank <franklin@bitsmasher.net>
2019-02-23 15:46:11 -0500

    downloader

--- my_resume.py
+++ my_resume.py
@@ -1,6 +1,8 @@
 import subprocess
-from flask import Flask, Response, render_template
+from flask import Flask, request, abort, jsonify, send_from_directory, Response, render_template
 import codecs
+
+DOWNLOADS = "/app/doc"
 
 my_resume = Flask(__name__,static_folder='doc')
 
@@ -8,9 +10,20 @@ my_resume = Flask(__name__,static_folder='doc')
 def render_static():
   return render_template('index.html', title = 'Franklin D. Resume')
 
-#@my_resume.route('/doc/<path:filename>', methods=['GET', 'POST'])
-#def download(filename):
-#  return send_from_directory(directory='doc', filename=filename)
+  @api.route("/files")
+  def list_files():
+    """Endpoint to list files on the server."""
+    files = []
+    for filename in os.listdir(DOWNLOADS):
+      path = os.path.join(DOWNLOADS, filename)
+      if os.path.isfile(path):
+        files.append(filename)
+    return jsonify(files)
+
+@api.route("/files/<path:path>")
+def get_file(path):
+  """Download a file."""
+  return send_from_directory(DOWNLOADS, path, as_attachment=True)
 
 @my_resume.errorhandler(404)
 def page_not_found(e):
