7a6bef21b73fbd2effa1f340a0ede8cb7c08850d
Franklin D <2730246+devsecfranklin@users.noreply.github.com>
2022-05-02 09:31:54 -0400

    disable bridgecrew (#150)
    * disable bridgecrew
    
    * disable bridgecrew

--- src/my_resume.py
+++ src/my_resume.py
@@ -7,46 +7,44 @@ app = Flask(__name__)
 
 
 def create_app(debug):
-  """Move app setup tasks into create_app so we can reference from test harness."""
-  app.debug = debug
-  app.secret_key = os.urandom(12)
-  return app
+    """Move app setup tasks into create_app so we can reference from test harness."""
+    app.debug = debug
+    app.secret_key = os.urandom(12)
+    return app
 
 
 @app.route("/")
 def render_static():
-  """Render the HTML page."""
-  return render_template("index.html", title="Franklin Diaz Resume")
+    """Render the HTML page."""
+    return render_template("index.html", title="Franklin Diaz Resume")
 
 
 @app.route("/pdf")
 def build_pdf():
-  """Generate a PDF file from HTML."""
-  #css = ["src/static/css/new-style.css"]
-  html = render_template("index.html")
-  return render_pdf(
-      HTML(string=html), download_filename="franklin_diaz_resume.pdf"
-  )
+    """Generate a PDF file from HTML."""
+    # css = ["src/static/css/new-style.css"]
+    html = render_template("index.html")
+    return render_pdf(HTML(string=html), download_filename="franklin_diaz_resume.pdf")
 
 
 @app.errorhandler(404)
 def page_not_found(my_err):
-  """Return a custom 404 error page."""
-  if request.method == "POST":
-      if request.form["submit_button"] == "Go Back to Resume":
-          return render_template("index.html", title="Franklin Diaz Resume")
-  elif request.method == "GET":
-      return render_template("404.html"), 404
-  else:
-      pass
-  return my_err
+    """Return a custom 404 error page."""
+    if request.method == "POST":
+        if request.form["submit_button"] == "Go Back to Resume":
+            return render_template("index.html", title="Franklin Diaz Resume")
+    elif request.method == "GET":
+        return render_template("404.html"), 404
+    else:
+        pass
+    return my_err
 
 
 if __name__ == "__main__":
-  create_app(debug=False)
-  # create app(debug=True)
-  app.run(host="127.0.0.1")
-  # app.run(host="0.0.0.0", port="5000")
+    create_app(debug=False)
+    # create app(debug=True)
+    app.run(host="127.0.0.1")
+    # app.run(host="0.0.0.0", port="5000")
 
 
 """my_resume application
