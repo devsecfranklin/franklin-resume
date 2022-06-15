913c0f2f9e1c24128cb81cda966179572314971a
FrankTheTank <2730246+devsecfranklin@users.noreply.github.com>
2022-06-15 10:10:44 -0400

    Testing clean up

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,13 +1,7 @@
-"""my_resume application
-
-    Test like so: python3 -m pytest tests/
-
-    :copyright: © 2021 by Franklin Diaz
-    :license: MIT
-"""
 import unittest
 
 import pytest
+
 from src.my_resume import create_app
 
 
@@ -31,3 +25,12 @@ def test_client():
     flaskr.app.config["TESTING"] = True
     client = flaskr.app.test_client()
     yield client
+
+
+"""my_resume application
+
+    Test like so: python3 -m pytest tests/
+
+    :copyright: © 2021 by Franklin Diaz
+    :license: MIT
+"""
--- tests/test_generate_index.py
+++ tests/test_generate_index.py
@@ -1,10 +1,10 @@
+
+def test_myview(client):
+    assert client.get("/").status_code == 200
+
 """my_resume application
 
   Test like so: python3 -m pytest tests/
   :copyright:  © 2021 by Franklin Diaz
   :license: MIT
 """
-
-
-def test_myview(client):
-    assert client.get("/").status_code == 200
