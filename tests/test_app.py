65ba2ae8973461516c0869c98c7680c9daf23428
Franklin D <2730246+devsecfranklin@users.noreply.github.com>
2022-06-15 09:43:46 -0400

    updates and autotools (#153)

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,3 +1,8 @@
+def test_create_app(app):
+    """Instantiate the application."""
+    assert app
+
+
 """my_resume application
 
 Test like so: python3 -m pytest tests/
@@ -5,8 +10,3 @@ Test like so: python3 -m pytest tests/
 :copyright: © 2021 by Franklin Diaz
 :license: MIT
 """
-
-
-def test_create_app(app):
-    """Instantiate the application."""
-    assert app
