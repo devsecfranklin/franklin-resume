60bba94e40aeb3dabf7e3f37d52d53fbf0e07b10
Franklin D <2730246+devsecfranklin@users.noreply.github.com>
2022-06-22 11:30:46 -0400

    work on test cases (#155)

--- tests/test_404.py
+++ tests/test_404.py
@@ -1,9 +1,7 @@
-"""my_resume application
+import unittest
+import pytest
 
-  Test like so: python3 -m pytest tests/
-  :copyright:  © 2021 by Franklin Diaz
-  :license: MIT
-"""
+
 from flask import request
 from werkzeug.datastructures import ImmutableMultiDict
 
@@ -13,3 +11,10 @@ def test_404(client):
     request.form = ImmutableMultiDict([("submit_button", "Go Back to Resume")])
     response = client.get("/garbage")
     assert response.status_code == 404
+
+"""my_resume application
+
+  Test like so: python3 -m pytest tests/
+  :copyright:  © 2021 by Franklin Diaz
+  :license: MIT
+"""
