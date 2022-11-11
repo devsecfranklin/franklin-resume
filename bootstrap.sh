bbf9b67b1a63d86ca1beabd966a2532d8d575892
franklin <2730246+devsecfranklin@users.noreply.github.com>
2022-11-10 21:18:12 -0500

    testing new bootstrap file

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-# 2/25/2022 Maintainer script 
+# v0.1 02/25/2022 Maintainer script
+# v0.2 09/24/2022 Update this script
+# v0.3 10/19/2022 Add tool functions
 
 # Author:  2730246+devsecfranklin@users.noreply.github.com 
 
@@ -23,6 +25,117 @@ YELLOW='\033[1;33m'
 NC='\033[0m' # No Color
 
 MY_OS="unknown"
+
+function run_autopoint() {
+  echo "Checking autopoint version..."
+  ver=`autopoint --version | awk '{print $NF; exit}'`
+  ap_maj=`echo $ver | sed 's;\..*;;g'`
+  ap_min=`echo $ver | sed -e 's;^[0-9]*\.;;g'  -e 's;\..*$;;g'`
+  ap_teeny=`echo $ver | sed -e 's;^[0-9]*\.[0-9]*\.;;g'`
+  echo "    $ver"
+
+  case $ap_maj in
+      0)
+          if test $ap_min -lt 14 ; then
+              echo "You must have gettext >= 0.14.0 but you seem to have $ver"
+              exit 1
+          fi
+          ;;
+  esac
+  echo "Running autopoint..."
+  autopoint --force || exit 1
+}
+
+function run_libtoolize() {
+  echo "Checking libtoolize version..."
+  libtoolize --version 2>&1 > /dev/null
+  rc=$?
+  if test $rc -ne 0 ; then
+      echo "Could not determine the version of libtool on your machine"
+      echo "libtool --version produced:"
+      libtool --version
+      exit 1
+  fi
+  lt_ver=`libtoolize --version | awk '{print $NF; exit}'`
+  lt_maj=`echo $lt_ver | sed 's;\..*;;g'`
+  lt_min=`echo $lt_ver | sed -e 's;^[0-9]*\.;;g'  -e 's;\..*$;;g'`
+  lt_teeny=`echo $lt_ver | sed -e 's;^[0-9]*\.[0-9]*\.;;g'`
+  echo "    $lt_ver"
+
+  case $lt_maj in
+      0)
+          echo "You must have libtool >= 1.4.0 but you seem to have $lt_ver"
+          exit 1
+    ;;
+
+      1)
+          if test $lt_min -lt 4 ; then
+              echo "You must have libtool >= 1.4.0 but you seem to have $lt_ver"
+              exit 1
+          fi
+          ;;
+
+      2)
+          ;;
+
+      *)
+          echo "You are running a newer libtool than gerbv has been tested with."
+    echo "It will probably work, but this is a warning that it may not."
+    ;;
+  esac
+  echo "Running libtoolize..."
+  libtoolize --force --copy --automake || exit 1
+}
+
+function run_aclocal() {
+  echo "Checking aclocal version..."
+  acl_ver=`aclocal --version | awk '{print $NF; exit}'`
+  echo "    $acl_ver"
+
+  echo "Running aclocal..."
+  #aclocal -I m4 $ACLOCAL_FLAGS || exit 1
+  aclocal -I config || exit 1
+  echo "... done with aclocal."
+}
+
+function run_autoheader() {
+  echo "Checking autoheader version..."
+  ah_ver=`autoheader --version | awk '{print $NF; exit}'`
+  echo "    $ah_ver"
+
+  echo "Running autoheader..."
+  autoheader || exit 1
+  echo "... done with autoheader."
+}
+
+function run_automake() {
+  echo "Checking automake version..."
+  am_ver=`automake --version | awk '{print $NF; exit}'`
+  echo "    $am_ver"
+
+  echo "Running automake..."
+  automake -a -c --add-missing || exit 1
+  #automake --force --copy --add-missing || exit 1
+  echo "... done with automake."
+}
+
+function run_autoconf() {
+  echo "Checking autoconf version..."
+  ac_ver=`autoconf --version | awk '{print $NF; exit}'`
+  echo "    $ac_ver"
+
+  echo "Running autoconf..."
+  autoconf || exit 1
+  echo "... done with autoconf."
+}
+
+function check_installed() {
+  if ! command -v ${1} &> /dev/null
+  then
+    echo "${1} could not be found"
+    exit
+  fi
+}
 
 function detect_os() {
     if [ "$(uname)" == "Darwin" ]
@@ -56,15 +169,11 @@ function macos() {
   brew install gawk
   if [ ! -f "./config.status" ]; then
     echo -e "${CYAN}Running libtool/autoconf/automake...${NC}"
-    # glibtoolize # for building shared libraries
+    # glibtoolize
     aclocal -I config
-    if [ -d "aclocal" ]; then
-      autoreconf
-    else
-      autoreconf -i
-    fi
+    autoreconf -i
     automake -a -c --add-missing
-    brew install az
+    #brew install az
   else
     echo -e "${CYAN}Your system is already configured. (Delete config.status to reconfigure)${NC}"
     ./config.status
@@ -72,35 +181,41 @@ function macos() {
   echo -e "${CYAN}HINT: now type \"./configure\"${NC}"
 }
 
-function debian {
-  # sudo apt install gnuplot gawk libtool psutils make
+function install_debian() {
+  declare -a  Packages=( "doxygen" "gawk" "doxygen-latex" )
 
-  if [ ! -f "./config.status" ]; then
-    # libtoolize
-    if [ -d "aclocal" ]; then
-      autoreconf
-    else
-      mkdir aclocal
-      aclocal -I config
-      autoreconf -i
+  for i in ${Packages[@]};
+  do
+    PKG_OK=$(dpkg-query -W --showformat='${Status}\n' ${i}|grep "install ok installed")
+    echo Checking for ${i}: $PKG_OK
+    if [ "" = "$PKG_OK" ]; then
+      echo "Installing ${i} since it is not found."
+      sudo apt-get --yes install ${i}
     fi
-    automake -a -c --add-missing
-    ./configure
-  else
-    ./config.status
-  fi
+  done
+}
+
+function debian() {
+  # sudo apt install gnuplot gawk libtool psutils make autopoint
+  # run_autopoint
+  if [ ! -d "aclocal" ]; then mkdir aclocal; fi
+  run_aclocal
+  autoreconf -i
+  run_automake
+  ./configure
+  #./config.status
 }
 
 function redhat() {
   if [ ! -f "./config.status" ]; then
-    if [ -d "aclocal" ]; then
-      autoreconf
-    else
-      mkdir aclocal
-      aclocal -I config
-      autoreconf -i
-    fi
-    automake -a -c --add-missing
+    # libtoolize
+    if [ ! -d "aclocal" ]; then mkdir aclocal; fi
+    mkdir aclocal
+    #aclocal -I config
+    run_aclocal
+    autoreconf -i
+    #automake -a -c --add-missing
+    run_automake
     ./configure
   else
     ./config.status
@@ -109,15 +224,20 @@ function redhat() {
 
 function main() {
   detect_os
+  #check_installed doxygen
+
   if [ "${MY_OS}" == "mac" ]; then
+    check_installed brew
     macos
   fi
   if [ "${MY_OS}" == "rh" ]; then
     redhat
   fi
   if [ "${MY_OS}" == "deb" ]; then
+    install_debian
     debian
   fi
 }
 
 main
+
