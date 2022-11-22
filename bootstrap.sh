e0904c02d64c3e3c6ca7bd41aed5e4c4aec94edb
franklin <2730246+devsecfranklin@users.noreply.github.com>
2022-11-22 12:33:56 -0500

    update again

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,9 +3,12 @@
 # v0.1 02/25/2022 Maintainer script
 # v0.2 09/24/2022 Update this script
 # v0.3 10/19/2022 Add tool functions
+# v0.4 11/10/2022 Add automake check
+# v0.5 11/15/2022 Handle Docker container builds
 
 # Author:  2730246+devsecfranklin@users.noreply.github.com 
 
+#set -eu
 
 #Black        0;30     Dark Gray     1;30
 #Red          0;31     Light Red     1;31
@@ -19,15 +22,26 @@
 RED='\033[0;31m'
 LRED='\033[1;31m'
 LGREEN='\033[1;32m'
+LBLUE='\033[1;34m'
 CYAN='\033[0;36m'
 LPURP='\033[1;35m'
 YELLOW='\033[1;33m'
 NC='\033[0m' # No Color
 
 MY_OS="unknown"
+CONTAINER=false
+DOCUMENTATION=false
+
+# Check if we are inside a docker container
+function check_docker() {
+  if [ -f /.dockerenv ]; then
+    echo -e "${LGREEN}Building in container...${NC}"
+    CONTAINER=true
+  fi
+}
 
 function run_autopoint() {
-  echo "Checking autopoint version..."
+  echo -e "${CYAN}Checking autopoint version...${NC}"
   ver=`autopoint --version | awk '{print $NF; exit}'`
   ap_maj=`echo $ver | sed 's;\..*;;g'`
   ap_min=`echo $ver | sed -e 's;^[0-9]*\.;;g'  -e 's;\..*$;;g'`
@@ -88,14 +102,14 @@ function run_libtoolize() {
 }
 
 function run_aclocal() {
-  echo "Checking aclocal version..."
+  echo -e "${LBLUE}Checking aclocal version...${NC}"
   acl_ver=`aclocal --version | awk '{print $NF; exit}'`
   echo "    $acl_ver"
 
-  echo "Running aclocal..."
+  echo -e "${CYAN}Running aclocal...${NC}"
   #aclocal -I m4 $ACLOCAL_FLAGS || exit 1
   aclocal -I config || exit 1
-  echo "... done with aclocal."
+  echo -e "${CYAN}.. done with aclocal.${NC}"
 }
 
 function run_autoheader() {
@@ -125,7 +139,7 @@ function run_autoconf() {
   echo "    $ac_ver"
 
   echo "Running autoconf..."
-  autoconf || exit 1
+  autoreconf -i || exit 1
   echo "... done with autoconf."
 }
 
@@ -160,83 +174,77 @@ function detect_os() {
     fi
 }
 
-function macos() {
+function install_macos() {
   echo -e "${CYAN}Updating brew for MacOS (this may take a while...)${NC}"
   brew cleanup
   brew upgrade
+
   echo -e "${CYAN}Setting up autools for MacOS (this may take a while...)${NC}"
   # brew install libtool
+  brew install automake
   brew install gawk
-  if [ ! -f "./config.status" ]; then
-    echo -e "${CYAN}Running libtool/autoconf/automake...${NC}"
-    # glibtoolize
-    aclocal -I config
-    autoreconf -i
-    automake -a -c --add-missing
-    #brew install az
-  else
-    echo -e "${CYAN}Your system is already configured. (Delete config.status to reconfigure)${NC}"
-    ./config.status
-  fi
-  echo -e "${CYAN}HINT: now type \"./configure\"${NC}"
 }
 
 function install_debian() {
-  declare -a  Packages=( "doxygen" "gawk" "doxygen-latex" )
+  # sudo apt install gnuplot gawk libtool psutils make autopoint
+  #declare -a  Packages=( "doxygen" "gawk" "doxygen-latex" "automake" )
+  declare -a Packages=( "git" "make" "automake" "libtool" )
+
+  # Container package installs will fail unless you do an initial update, the upgrade is optional
+  if [ "${CONTAINER}" = true ]; then
+    apt-get update && apt-get upgrade -y
+  fi
 
   for i in ${Packages[@]};
   do
-    PKG_OK=$(dpkg-query -W --showformat='${Status}\n' ${i}|grep "install ok installed")
-    echo Checking for ${i}: $PKG_OK
-    if [ "" = "$PKG_OK" ]; then
-      echo "Installing ${i} since it is not found."
-      sudo apt-get --yes install ${i}
+    PKG_OK=$(dpkg-query -W --showformat='${Status}\n' ${i}|grep "install ok installed") &> /dev/null
+    # echo -e "${LBLUE}Checking for ${i}: ${PKG_OK}${NC}"
+    if [ "" = "${PKG_OK}" ]; then
+      echo -e "${LBLUE}Installing ${i} since it is not found.${NC}"
+
+      # If we are in a container there is no sudo in Debian
+      if [ "${CONTAINER}" = true ]; then
+        apt-get --yes install ${i}
+      else
+        sudo apt-get install ${i} -y
+      fi
     fi
   done
 }
 
-function debian() {
-  # sudo apt install gnuplot gawk libtool psutils make autopoint
-  # run_autopoint
-  if [ ! -d "aclocal" ]; then mkdir aclocal; fi
-  run_aclocal
-  autoreconf -i
-  run_automake
-  ./configure
-  #./config.status
-}
-
-function redhat() {
-  if [ ! -f "./config.status" ]; then
-    # libtoolize
-    if [ ! -d "aclocal" ]; then mkdir aclocal; fi
-    mkdir aclocal
-    #aclocal -I config
-    run_aclocal
-    autoreconf -i
-    #automake -a -c --add-missing
-    run_automake
-    ./configure
-  else
-    ./config.status
-  fi    
+function install_redhat() {
+  # Container package installs will fail unless you do an initial update, the upgrade is optional
+  if [ "${CONTAINER}" = true ]; then
+    sudo yum update
+  fi 
 }
 
 function main() {
+  check_docker
   detect_os
   #check_installed doxygen
 
+  if [ ! -d "config/m4" ]; then mkdir -p config/m4; fi
+
   if [ "${MY_OS}" == "mac" ]; then
     check_installed brew
-    macos
+    install_macos
   fi
+
   if [ "${MY_OS}" == "rh" ]; then
-    redhat
+    install_redhat
   fi
+
   if [ "${MY_OS}" == "deb" ]; then
     install_debian
-    debian
   fi
+
+  if [ ! -d "aclocal" ]; then mkdir aclocal; fi
+  run_aclocal
+  run_autoconf
+  run_automake
+  ./configure
+  #./config.status
 }
 
 main
