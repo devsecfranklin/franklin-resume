5c903f0a2b424013cef9f95a5bb7e7d0835cffdc
Franklin D <2730246+devsecfranklin@users.noreply.github.com>
2023-03-02 18:31:04 -0500

    update whitelist (#212)
    * update whitelist
    
    * update markel firewalls
    
    * update access list

--- terraform/lab_backend/gcp.tf
+++ terraform/lab_backend/gcp.tf
@@ -38,6 +38,7 @@ resource "google_compute_firewall" "lab-ingress" {
   source_ranges = [
     "10.199.0.0/21",     # Azure common subnet
     "68.38.137.81/32",   # Franklin lab
+    "174.160.179.231",   # Raneesh
     "34.134.31.136/32",  # ps-devsecops-panorama-three-10-0-4 
     "34.136.90.64/32",   # ps-devsecops-panorama-two-10-0-4
     "52.151.200.84/32",  # Azure lab firewall number one
@@ -45,10 +46,17 @@ resource "google_compute_firewall" "lab-ingress" {
     "52.151.200.153/32", # azure MTA 3
     "52.151.200.97/32",  # azure MTA 4
     "34.66.44.164/32",   # gke cluster
-    "35.232.129.131/32",   # gcp ps-devsecops-fw01
+    "35.232.129.131/32", # gcp ps-devsecops-fw01
     "137.83.195.1/32",   # Palo Corp Global Protect
     "137.83.195.1/32",   # Palo Corp Global Protect
-    "20.238.77.180",     # azure testing - markel
-    "20.238.77.140"      # azure testing markel
+    "137.83.195.96/32",  # Palo Corp Global Protect
+    "20.22.238.55/32",   # azure testing - markel
+    "20.22.238.56/32",   # azure testing markel
+    "52.165.18.208/32",  # azure markel
+    "52.165.18.223/32",  # azure markel
+    "20.126.113.109/32", # AZURE MARKEL
+    "20.126.113.103/32", # azure markel
+    "74.234.110.238",    # Azure Markel
+    "68.219.104.166"     # Azure markel
   ]
 }
