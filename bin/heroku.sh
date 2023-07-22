be070c76eafb008ae454648b5eef76fa259b89c5
FrankTheTank <2730246+devsecfranklin@users.noreply.github.com>
2023-07-21 22:53:56 -0400

    update requirements

--- bin/heroku.sh
+++ bin/heroku.sh
@@ -11,5 +11,9 @@ heroku ps -a franklin-resume
 
 heroku buildpacks -a franklin-resume
 
+# run a web dyno for this app
+# heroku ps:scale web=1 -a franklin-resume
+
 #heroku logs --tail -a franklin-resume
 heroku logs -a franklin-resume
+
