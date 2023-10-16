b192a6d96861d4037ea756828f80562572136782
Franklin D <2730246+devsecfranklin@users.noreply.github.com>
2023-10-16 17:50:12 -0400

    add route for bio page (#236)
    * add route for bio page
    
    * add route for bio page
    
    * add route for bio page

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 import setuptools
 
 # Package meta-data.
-NAME = 'franklin-resume'
-DESCRIPTION = 'Resume Application'
-URL = 'https://github.com/devsecfranklin/franklin-resume'
-EMAIL = 'franklin@dead10c5.org'
-AUTHOR = 'Franklin Diaz'
-REQUIRES_PYTHON = '>=3.10.0'
-VERSION = '0.4'
+NAME = "franklin-resume"
+DESCRIPTION = "Resume Application"
+URL = "https://github.com/devsecfranklin/franklin-resume"
+EMAIL = "franklin@dead10c5.org"
+AUTHOR = "Franklin Diaz"
+REQUIRES_PYTHON = ">=3.10.0"
+VERSION = "0.4"
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
@@ -18,10 +18,10 @@ setuptools.setup(
     version=VERSION,
     description=DESCRIPTION,
     long_description=long_description,
-    long_description_content_type='text/markdown',
+    long_description_content_type="text/markdown",
     author=AUTHOR,
     author_email=EMAIL,
     python_requires=REQUIRES_PYTHON,
     url=URL,
-    license='MIT',
+    license="MIT",
 )
