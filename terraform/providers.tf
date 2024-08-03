b56f043f7af0d238aaf1dcefa736b95b298b7416
FrankTheTank <2730246+devsecfranklin@users.noreply.github.com>
2024-08-03 14:15:06 -0600

    testing GCP cloud function

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,3 +1,7 @@
+# SPDX-FileCopyrightText: 2023 DE:AD:10:C5 <franklin@dead10c5.org>
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
 terraform {
   required_providers {
     google = {
@@ -10,4 +14,11 @@ terraform {
     prefix = "cloud-function/state"
     //credentials = "credentials.json"
   }
+}
+
+provider "google" {
+  project = var.project_id
+  region  = var.region
+  zone    = var.zone
+  //impersonate_service_account = var.service_account_terraform
 }
