ff3578ae191eae10183bc8cfa6b4d124d63d423c
Franklin D. <2730246+devsecfranklin@users.noreply.github.com>
2024-11-22 00:06:47 -0700

    cluster build updates (#999)
    * cluster updates
    
    * checkpoint
    
    * update the autoconf
    
    * update the autoconf

--- assembly/src/asm_hello.asm
+++ assembly/src/asm_hello.asm
@@ -8,12 +8,6 @@ section .text
 global _start
 _start:
     mov rax, 1
-; asm_hello.asm:10: error: instruction not supported in 32-bit mode
-; asm_hello.asm:11: error: instruction not supported in 32-bit mode
-; asm_hello.asm:12: error: instruction not supported in 32-bit mode
-; asm_hello.asm:13: error: instruction not supported in 32-bit mode
-; asm_hello.asm:16: error: instruction not supported in 32-bit mode
-; asm_hello.asm:17: error: instruction not supported in 32-bit mode
     mov rdi, 1
     mov rsi, msg
     mov rdx, msgLen
