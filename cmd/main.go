cd43b3caf188f2d2139eef22d0dc01e224b28998
franklin <frank378@gmail.com>
2025-11-23 19:48:18 -0700

    add my logging module

--- cmd/main.go
+++ cmd/main.go
@@ -20,10 +20,29 @@ var (
 
 func main() {
 	fs := http.FileServer(http.Dir("./static/www"))
-	http.Handle("/static/www/", http.StripPrefix("/static/www/", fs))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
 
 	tmpls, err = template.ParseGlob(LayoutDir + "/*.tmpl")
 	if err != nil {
 		panic(err)
+	}
+
+	http.HandleFunc("/", handler)
+
+}
+
+func handler(w http.ResponseWriter, r *http.Request) { // handler for the root path
+	log.Println("Serving index page")
+
+	w.Header().Set("Cache-Control", "no-cache, no-store, must-revalidate")
+	w.Header().Set("Pragma", "no-cache")
+	w.Header().Set("Expires", "0")
+
+	page := Page{"www.bitsmasher.net"}
+
+	err := tmpls.ExecuteTemplate(w, "indexPage", page) // Assuming you have an "indexPage" template
+	if err != nil {
+		log.Println(err)
+		http.Error(w, "Internal server error: Could not render index page.", http.StatusInternalServerError)
 	}
 }
