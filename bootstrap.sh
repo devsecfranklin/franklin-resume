6f33ef2ab8b4a3e4889ac2c3b5ca3b991aaa497a
franklin <frank378@gmail.com>
2026-01-29 13:36:17 -0700

    updating latex build files

---
+++ bootstrap.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+#
+# SPDX-FileCopyrightText: ©2026 franklin <smoooth.y62wj@passmail.net>
+#
+# SPDX-License-Identifier: MIT
+
+# ChangeLog:
+
+DEB_PKG=(libpcsclite-dev texlive-pictures texlive-latex-extra)
+LRED=$(tput setaf 1)
+
+function main() {
+  # echo -e "${LRED}can not find common.sh${NC}"
+
+  autoreconf -i
+  ./configure
+}
+
+main "$@"
